80f7f9f0ff40e8222264c243e4e24fe80f8b9adc
Raabb Ajam <raabbajam@gmail.com>
2016-02-08 14:20:33 +0700

    Added proof of concept, 33% succeed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,71 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
+import path from 'path';
+
 /**
  * @param {Type}
  * @return {Type}
  */
-export default function () {
+export default function (settings) {
+  const defaults = {
+    deleteTemporaryImage: true,
+    deleteSourceImage: false,
+    resolveBeforeCleaning: true,
+  };
+  const options = Object.assign({}, settings, defaults);
   return (imagePath) => new Promise((resolve, reject) => {
-    exec(`tesseract ${imagePath} stdout digits`, (error, text) => {
+    const {convertCommand, imageTmpPath} =
+      generateConvertCommandAndOutputPath(imagePath, options.transforms);
+    exec(convertCommand, (error) => {
       if (error) {
         return reject(error);
       }
-      text = text.trim();
-      return resolve(text);
+      exec(`tesseract ${imageTmpPath} stdout\
+        ${options.digitOnly ? ' digits' : ''}`, (error2, text) => {
+        if (error2) {
+          return reject(error2);
+        }
+        text = text.trim();
+        if (options.digitOnly) {
+          text = text.replace(/[^A-z\d]/, '');
+        }
+        if (options.resolveBeforeCleaning) {
+          resolve(text);
+        }
+        return cleaning({options, imagePath, imageTmpPath})
+          .then(() => {
+            const resolveAfterCleaning = !options.resolveBeforeCleaning;
+            if (resolveAfterCleaning) {
+              return resolve(text);
+            }
+          });
+      });
     });
   });
+}
+
+function generateConvertCommandAndOutputPath(imagePath, transforms) {
+  const imageName = path.basename(imagePath);
+  const imageTmpName = imageName.replace(path.extname(imageName), '.tiff');
+  const imageTmpPath = path.resolve(__dirname, '.tmp', imageTmpName);
+  transforms = transforms.map((option) =>
+    `-${option.key} ${option.value || ''}`.trim());
+  const convertCommand = ['convert', imagePath, ...transforms, imageTmpPath].join(' ');
+  return {convertCommand, imageTmpPath};
+}
+
+function cleaning({options, imagePath, imageTmpPath}) {
+  const actions = [];
+  if (options.deleteSourceImage) {
+    actions.push(deleteFile(imagePath));
+  }
+  if (options.deleteTemporaryImage) {
+    actions.push(deleteFile(imageTmpPath));
+  }
+  return actions.length ?
+    Promise.all(actions) :
+    Promise.resolve();
+}
+
+function deleteFile() {
+  return Promise.resolve();
 }
--- test/index.js
+++ test/index.js
@@ -1,24 +1,46 @@
 import test from 'tape';
 import catcha from '../src';
-import { getTestData } from './utils';
+import {getTestData} from './utils';
 
 test('catcha test -- easy module', (t) => {
-  const settings = {};
+  const settings = {
+    transforms: [
+      {key: 'resize', value: '240x100'},
+      {key: 'colorspace', value: 'gray'},
+      {key: 'negate'},
+      {key: 'threshold', value: '12%'},
+      {key: 'blur', value: '10 '},
+      {key: 'threshold', value: '43%'},
+      {key: 'negate'},
+    ],
+    digitOnly: true,
+    deleteTemporaryImage: false,
+  };
   const kitten = catcha(settings);
   getTestData('easy')
     .then((testData) => {
       t.plan(testData.length);
-      testData.forEach((image) => {
+      return Promise.all(testData.map((image) =>
         kitten(image.path)
           .then((text) => {
             t.equal(text, image.text);
-          });
-      });
-    });
+          })));
+    })
+    .catch(t.end);
 });
 
 test('catcha test -- intermediate module', (t) => {
-  const settings = {};
+  const settings = {
+    transforms: [
+      {key: 'resize', value: '120'},
+      {key: 'threshold', value: '5%'},
+      {key: 'blur', value: '20'},
+      {key: 'threshold', value: '35%'},
+      {key: 'blur', value: '1'},
+    ],
+    digitOnly: true,
+    deleteTemporaryImage: false,
+  };
   const kitten = catcha(settings);
   getTestData('intermediate')
     .then((testData) => {
